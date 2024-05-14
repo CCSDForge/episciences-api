8f048d6e383910b0b1fe3b01a1101f2c62c07bc5
Raphaël Tournoy <rtournoy@users.noreply.github.com>
2024-05-14 11:12:52 +0200

    force php8.2 + --ignore-platform-reqs

--- deployVersion.sh
+++ deployVersion.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-PHP_BIN="/usr/bin/php"
+PHP_BIN="/usr/bin/php8.2"
 deployDate=$(date "+%Y-%m-%d %X %z")
 git fetch --all
 git fetch --tags
@@ -8,7 +8,7 @@ currentTag=$(git describe --tags --abbrev=0 $1)
 echo '<?php' > 'version.php';
 echo "\$appVersion='$currentTag ($deployDate)';" >>  'version.php';
 
-$PHP_BIN composer.phar install -o --no-dev
+$PHP_BIN composer.phar install -o --no-dev --ignore-platform-reqs
 $PHP_BIN composer dump-env production
 yarn ## Running yarn with no command will run yarn install
 yarn encore production
