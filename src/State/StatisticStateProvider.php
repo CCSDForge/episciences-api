d249375773ff6f0a11d86af01e52c2a1fb0b6201
Djamel Chibane <20502173+devletech@users.noreply.github.com>
2024-07-10 09:36:33 +0200

    fix typo.

--- src/State/StatisticStateProvider.php
+++ src/State/StatisticStateProvider.php
@@ -178,9 +178,9 @@ class StatisticStateProvider extends AbstractStateDataProvider implements Provid
             return null;
         }
 
-        $allaAcceptedArticle = $this->entityManagerInterface->getRepository(PaperLog::class)->getNumberOfAcceptedArticlesQuery($rvId, $years, $startAfterDate);
+        $allAcceptedArticle = $this->entityManagerInterface->getRepository(PaperLog::class)->getNumberOfAcceptedArticlesQuery($rvId, $years, $startAfterDate);
 
-        return round(($allaAcceptedArticle / $allSubmissions) * 100, 2);
+        return round(($allAcceptedArticle / $allSubmissions) * 100, 2);
 
     }
 }
