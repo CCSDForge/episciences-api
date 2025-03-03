d0da7bb0e0f768ea1c6460a0fc5771e85834628a
Djamel Chibane <20502173+devletech@users.noreply.github.com>
2025-03-03 16:18:41 +0100

    Updated deployVersion.sh

--- deployVersion.sh
+++ deployVersion.sh
@@ -3,13 +3,18 @@ PHP_BIN="/usr/bin/php8.2"
 deployDate=$(date "+%Y-%m-%d %X %z")
 git fetch --all
 git fetch --tags
-git checkout $1
-currentTag=$(git describe --tags --abbrev=0 $1)
+
+git checkout ${1:-main} ## default $1=main
+currentTag=$(git describe --tags --abbrev=0) ## get the most recent tag
+
+if test $1 != 'main'; then
+  currentTag=$1
+fi
+
 echo '<?php' > 'version.php';
 echo "\$appVersion='$currentTag ($deployDate)';" >>  'version.php';
 
 $PHP_BIN composer.phar install -o --no-dev --ignore-platform-reqs
-$PHP_BIN composer dump-env production
+$PHP_BIN composer.phar dump-env production
 yarn ## Running yarn with no command will run yarn install
 yarn encore production
-
