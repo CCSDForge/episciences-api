8ac51d8c1d73bfb1f7eab971ef46a3d27025bfdc
Djamel Chibane <20502173+devletech@users.noreply.github.com>
2025-12-08 08:39:55 +0100

    New version to deployVersion.sh: More readable; Performance; Explicit default value; Run webpack and composer per environment.

--- deployVersion.sh
+++ deployVersion.sh
@@ -4,18 +4,37 @@ deployDate=$(date "+%Y-%m-%d %X %z")
 git fetch --all
 git fetch --tags
 
-git checkout ${1:-main} ## default $1=main
-git pull
+#########################################
+## If no parameters are specified, the code is deployed from the main branch by default [$1=main].
+currentEnv=${1:-main}
+#########################################
+encoreEnv="production"
+composerInstallOptions="-o --no-dev --ignore-platform-reqs"
 currentTag=$(git describe --tags --abbrev=0) ## get the most recent tag
 
-if test $1 != 'main'; then
-  currentTag=$1
-fi
+case "$currentEnv" in
+    "main")
+        # nothing to do for main
+        ;;
+    "develop"|"dev")
+        composerInstallOptions=""
+        currentEnv="develop"
+        encoreEnv="dev"
+        currentTag="$currentEnv"
+        ;;
+    *)
+        currentTag="$currentEnv"
+        ;;
+esac
 
 echo '<?php' > 'version.php';
 echo "\$appVersion='$currentTag ($deployDate)';" >>  'version.php';
 
-$PHP_BIN composer.phar install -o --no-dev --ignore-platform-reqs
-$PHP_BIN composer.phar dump-env production
+git checkout $currentEnv
+git pull
+
+$PHP_BIN composer.phar dump-env $currentEnv
+$PHP_BIN composer.phar install $composerInstallOptions
+
 yarn ## Running yarn with no command will run yarn install
-yarn encore production
+yarn encore  $encoreEnv
