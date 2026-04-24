8769690dc4a9cf08d8715883de2da59ebe1b6d03
Raphaël Tournoy <rtournoy@users.noreply.github.com>
2026-04-24 11:27:24 +0200

    chore: refine Rector configuration for migration phase

--- rector.php
+++ rector.php
@@ -3,6 +3,8 @@
 declare(strict_types=1);
 
 use Rector\Config\RectorConfig;
+use Rector\Doctrine\Set\DoctrineSetList;
+use Rector\Symfony\Set\SymfonySetList;
 
 return RectorConfig::configure()
     ->withPaths([
@@ -14,24 +16,20 @@ return RectorConfig::configure()
     ])
     // Target PHP 8.3
     ->withPhpSets(php83: true)
-    ->withSymfonySets(
-        symfony70: true,
-        annotationsToAttributes: true,
-        codeQuality: true,
-        constructorInjection: true,
-    )
-    ->withDoctrineSets(
-        orm214: true,
-        annotationsToAttributes: true,
-        codeQuality: true,
-    )
+    ->withAttributesSets(symfony: true, doctrine: true)
+    ->withComposerBased(doctrine: true, symfony: true)
+    ->withSets([
+        DoctrineSetList::DOCTRINE_CODE_QUALITY,
+        SymfonySetList::SYMFONY_CODE_QUALITY,
+        SymfonySetList::SYMFONY_CONSTRUCTOR_INJECTION,
+    ])
     // Register Symfony container for advanced rules (uncomment if var/cache/dev exists)
     ->withPreparedSets(
-        deadCode: true,
+        deadCode: false,
         codeQuality: true,
         typeDeclarations: true,
-        privatization: true,
+        privatization: false,
         instanceof: true,
-        earlyReturn: true,
+        earlyReturn: false,
         strictBooleans: true
     );
